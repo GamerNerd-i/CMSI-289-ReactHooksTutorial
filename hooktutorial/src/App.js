c71d9fb9b4f652f40bb23b294b09d42f283013c4
Aidan Dionisio <aidanbcd@mac.com>
2021-01-28 14:57:39 -0800

    Video 1 result

--- hooktutorial/src/App.js
+++ hooktutorial/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
+import Button from "./Button"
+
 import './App.css';
 
+function initCount() {
+  console.log("initial count")
+  return 0;
+}
+
 function App() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Aidan')
+  
+  function handleClick(value) {
+    setCount((prevState) => {
+      return prevState + value;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{name}</div>
+      <div>{count}</div>
+      <Button handleClick = {handleClick} title = "+" value = {1}/>
+      <Button handleClick = {handleClick} title = "-" value = {-1}/>
+      {/* <button onClick = {() => setState(0)}>Reset</button> */}
     </div>
   );
 }
