0e9de41ee6817cc88769777641d1631b5a933c02
Aidan Dionisio <aidanbcd@mac.com>
2021-01-28 22:43:09 -0800

    Began blog

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+  <Row className="justify-content-md-center">
+    <Col xs lg="2">
+      <hi>Hello Aidan!</hi>
+    </Col>
+    <Col md="auto">Variable width content</Col>
+    <Col xs lg="2">
+      3 of 3
+    </Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col md="auto">Variable width content</Col>
+    <Col xs lg="2">
+      3 of 3
+    </Col>
+  </Row>
+</Container>
   );
 }
 
